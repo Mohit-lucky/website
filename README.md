b76172e410a003c4b821821bbdce4e659729044d
penny <penny@penny.lgbt>
2017-05-14 00:50:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+\# choo-animals-starter
+
+🌐 Made by [@louiscenter](https://twitter.com/louiscenter) 🌐
+
+✨ A starter app for the `your-first-choo-app` guide ✨ 
+
+First, remix this Glitch app so you can start editing the code.
+
+Once your new remix is open, select `index.js` in the left hand sidebar to get started.
+
+Make sure to follow the tutorial:
+
+[https://handbook.choo.io/your-first-choo-app](https://handbook.choo.io/your-first-choo-app)
+
---
+++ index.js
@@ -0,0 +1,63 @@
+var choo = require('choo')
+var html = require('choo/html')
+var css = require('sheetify')
+var GitHub = require('github-api')
+
+var app = choo()
+var gh = new GitHub()
+
+css('tachyons')
+
+function renderRepositories (state, emit) {
+  return html`
+    <ul class="list pv1 ph0" onload=${onload}>
+      ${state.repos.map(function (repo) {
+        return html`
+          <li class="db mr2">
+            <a href="" class="f6 f5-ns b dib pa2 link dim mid-gray">${repo.name}</a>
+            <p class="f7 i dib">${repo.description}</p>
+          </li>
+        `
+      })}
+    </ul>
+  `
+
+  function onload () {
+    emit('loadRepos')
+  }
+}
+
+function repoStore (state, emitter) {
+  state.repos = []
+  emitter.on('loadRepos', function () {
+    var org = gh.getOrganization('fruitfuljs')
+    org.getRepos().then(function (repos) {
+      console.log(repos.data[0])
+      state.repos = repos.data
+      emitter.emit('render')
+    })
+  })
+}
+
+function main (state, emit) {
+  return html`
+    <div class="ph3 ph5-ns sans-serif">
+      <img src="https://cdn.glitch.com/4b38f914-ed30-47a5-9476-1444ff542c52%2Flogo.png?1494707716957" class="v-mid" width=75 />
+      <h1 class="f1 dib v-mid pl3">fruitful.js</h1>
+
+      <p class="measure lh-copy">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+        vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+
+      <h1 class="f3 mb0">Repositories</h1>
+      ${renderRepositories(state, emit)}
+    </div>
+  `
+}
+
+app.route('/', main)
+app.use(repoStore)
+app.mount('div')
