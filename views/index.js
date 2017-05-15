9e95f50c5366bad564242d8dbc21b4027ce1abca
penny <penny@penny.lgbt>
2017-05-15 11:45:55 +0200

    add some copy text

--- views/index.js
+++ views/index.js
@@ -7,10 +7,15 @@ function view (state, emit) {
     return html`
       <div>
         <p class="measure lh-copy">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-          vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-          no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          Fruitful is a community of volunteers who are joining forces to
+          maintain adopted or abandoned JavaScript packages. We strongly value
+          collaboration when maintaining packages, and this is reflected in our
+          work.
+        </p>
+        <p class="measure lh-copy">
+          One of our fundamental principles is that everyone is welcome. We
+          pride ourselves in being an inclusive project, and every contributor
+          is invited into the organization.
         </p>
 
         <h1 class="f3 mb0">Repositories</h1>
