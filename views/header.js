6e5a6108bee54e6da7afcf1c08c93cdaf29ec25e
penny <penny@penny.lgbt>
2017-05-15 17:09:02 +0200

    redirect to hash routes in header

--- views/header.js
+++ views/header.js
@@ -4,7 +4,7 @@ function view (state, emit) {
   return html`
     <div class="mb4">
       <a href="/" class="f4 i fw2 link hover-blue no-underline black dib pv1 pr3 pointer outline-0">home</a>
-      <a href="/conduct" class="f4 i fw2 link hover-blue no-underline black dib pv1 pointer outline-0">conduct</a>
+      <a href="/#conduct" class="f4 i fw2 link hover-blue no-underline black dib pv1 pointer outline-0">conduct</a>
     </div>
   `
 }
