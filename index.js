714b806ffb89dcbdf6caecda535e82e92d6d427a
penny <penny@penny.lgbt>
2017-05-17 21:47:53 +0200

    fix linting issues

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var choo = require('choo')
-var html = require('choo/html')
 var css = require('sheetify')
-var GitHub = require('github-api')
 
 var indexView = require('./views/index')
 var conductView = require('./views/conduct')
 var store = require('./store')
 
 var app = choo()
-var gh = new GitHub()
 
 css('tachyons')
 css('text-spinners')
--- store.js
+++ store.js
@@ -4,7 +4,7 @@ var gh = new GitHub()
 function store (state, emitter) {
   state.repos = []
   state.reposLoading = true
-  
+
   emitter.on('loadRepos', function () {
     var org = gh.getOrganization('fruitfuljs')
     org.getRepos().then(function (repos) {
