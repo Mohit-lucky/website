3433a808f7e856a17b45b62850d06360948ed52b
penny <penny@penny.lgbt>
2017-05-23 23:34:13 +0200

    make header links more readable

--- views/header.js
+++ views/header.js
@@ -2,9 +2,10 @@ var html = require('choo/html')
 
 function view (state, emit) {
   return html`
-    <div class="mb4">
-      <a href="/" class="f4 i fw2 link hover-blue no-underline black dib pv1 pr3 pointer outline-0">home</a>
-      <a href="/#conduct" class="f4 i fw2 link hover-blue no-underline black dib pv1 pointer outline-0">conduct</a>
+    <div>
+      <a href="/" class="f3 fw6 link hover-blue no-underline black dib pv1 mr3 pointer outline-0">home</a>
+      <a href="/#conduct" class="f3 fw6 link hover-blue no-underline black dib pv1 mr3 pointer outline-0">conduct</a>
+      <a href="/#docs" class="f3 fw6 link hover-blue no-underline black dib pv1 pointer outline-0">docs</a>
     </div>
   `
 }
