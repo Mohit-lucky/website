00023e18c56e5dc748ca50b2472c2718eb7a1a6b
penny <penny@penny.lgbt>
2017-05-26 16:30:49 +0200

    add info about coc contact methods

--- views/conduct.js
+++ views/conduct.js
@@ -22,6 +22,12 @@ function view (state, emit) {
           We take enforcement very seriously. If anything ever happens that
           might violate the Code of Conduct, feel free to contact any of us.
         </p>
+        <p class="measure lh-copy">
+          To contact the organization administrators, you should send an email
+          to <a href="mailto:coc@fruitfuljs.org">coc@fruitfuljs.org</a>. This
+          is the recommended method, however you may also contact one of the
+          administrators in private if possible.
+        </p>
       </div>
     `
   })
